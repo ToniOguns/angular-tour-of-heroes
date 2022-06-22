d91a15a01fb93f8c7c726e47a87ca889daa93fc1
Oluwatoni Ogunfusika <ogunfusika64@gmail.com>
2022-06-22 09:36:59 +0100

    Chore: Implemented In Memory DB service and getting Data from a Server

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -8,7 +8,7 @@ import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: [ './hero-detail.component.css' ]
+  styleUrls: [ './hero-detail.component.scss' ]
 })
 export class HeroDetailComponent implements OnInit {
   hero: Hero | undefined;
@@ -24,12 +24,19 @@ export class HeroDetailComponent implements OnInit {
   }
 
   getHero(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
   }
 }
